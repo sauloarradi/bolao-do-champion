c8c21deb13c4ca0f3f4bd63c743ae06d6882ac13
Saulo Arradi <71353883+sauloarradi@users.noreply.github.com>
2026-05-23 23:21:42 -0300

    Delete backend directory

--- backend/lib/teams.js
+++
@@ -1,4 +0,0 @@
-export const TEAMS = new Set([
-  'bra','arg','fra','esp','eng','ger','por','ned','uru','bel','usa','mex',
-  'col','mar','jpn','sen','den','cro','sui','aus','ecu','kor','cam','irn'
-]);
